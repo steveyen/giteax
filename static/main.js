5eaafd59b0a4a2188c4334f6411e51f4ba7d61b8
Steve Yen <steve.yen@gmail.com>
2020-09-25 19:03:57 -0700

    placeholder cluster-config template

--- static/main.js
+++ static/main.js
@@ -2,4 +2,13 @@ console.log("xmain start");
 
 $(document).ready(async () => {
   console.log("xmain ready");
+
+  var rt = document.getElementById("repo-topics");
+  var tmpl = document.getElementById("template-cluster-config");
+  if (rt && tmpl) {
+    el = document.createElement("div");
+    el.innerHTML = tmpl.innerHTML;
+
+    rt.parentElement.insertBefore(el, rt.nextSibling);
+  }
 });
