30645afe26173afaa402608582efb9ef03c23745
Steve Yen <steve.yen@gmail.com>
2020-09-26 11:21:27 -0700

    body.x-none when no giteax is not engaged

--- static/main.js
+++ static/main.js
@@ -1,30 +1,40 @@
 console.log("xmain start");
 
 $(document).ready(async () => {
-  console.log("xmain ready");
+  console.log("xmain ready...");
 
-  var rt = document.getElementById("repo-topics");
-  if (rt) {
-    var lb = document.createElement("label");
-    lb.className = "x";
-    lb.htmlFor = "x-repo-advanced-toggle";
-    lb.innerHTML = "Show details / history";
-    rt.parentElement.insertBefore(lb, rt.nextSibling);
+  if (document.getElementById("repo-files-table")) {
+    var rt = document.getElementById("repo-topics");
+    if (rt) {
+      var lb = document.createElement("label");
+      lb.className = "x";
+      lb.htmlFor = "x-repo-advanced-toggle";
+      lb.innerHTML = "Show details / history";
+      rt.parentElement.insertBefore(lb, rt.nextSibling);
 
-    var cb = document.createElement("input");
-    cb.className = "x";
-    cb.id = "x-repo-advanced-toggle";
-    cb.name = "x-repo-advanced-toggle";
-    cb.type = "checkbox";
-    rt.parentElement.insertBefore(cb, rt.nextSibling);
+      var cb = document.createElement("input");
+      cb.className = "x";
+      cb.id = "x-repo-advanced-toggle";
+      cb.name = "x-repo-advanced-toggle";
+      cb.type = "checkbox";
+      rt.parentElement.insertBefore(cb, rt.nextSibling);
 
-    var tmpl = document.getElementById("template-cluster-info");
-    if (tmpl) {
-      el = document.createElement("div");
-      el.className = "x";
-      el.innerHTML = tmpl.innerHTML;
+      var tmpl = document.getElementById("template-cluster-info");
+      if (tmpl) {
+        el = document.createElement("div");
+        el.className = "x";
+        el.innerHTML = tmpl.innerHTML;
 
-      rt.parentElement.insertBefore(el, rt.nextSibling);
+        rt.parentElement.insertBefore(el, rt.nextSibling);
+
+        console.log("xmain ready... done");
+
+        return;
+      }
     }
   }
+
+  document.body.className = document.body.className + " x-none";
+
+  console.log("xmain ready... done (disabled)");
 });
